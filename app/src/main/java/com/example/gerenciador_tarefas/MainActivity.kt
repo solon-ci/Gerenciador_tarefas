c47910827a6934881344f5ddf4de57aa4e6343eb
solonm <solon-ci>
2025-08-13 11:14:53 -0300

    Criação do navigation, alterando os arquivos bild.grandle.tks;grandle-wrapper.properties; libs.version.toml; criação da função MainScreen que será responsável por gerenciar o navigation

--- app/src/main/java/com/example/gerenciador_tarefas/MainActivity.kt
+++ app/src/main/java/com/example/gerenciador_tarefas/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.gerenciador_tarefas
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.gerenciador_tarefas.ui.theme.Gerenciador_tarefasTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +27,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Gerenciador_tarefasTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Gerenciador_tarefasTheme {
-        Greeting("Android")
-    }
-}
+fun MainScreen(){}
