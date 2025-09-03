74c2171134c2ebb414b25db8f056cd62c016b21e
solonm <solon-ci>
2025-09-03 15:06:02 -0300

    Esta feature contém o projeto finalizado com os recursos de check para mostrar se a tarefa foi finalizada, além de conter a lista de tarefas, um botão para exclusão do item da lista e a possibilidade de quando clicar aparecer um alert contendo a descrição do item

--- app/src/main/java/com/example/gerenciador_tarefas/useri/TarefaItem.kt
+++ app/src/main/java/com/example/gerenciador_tarefas/useri/TarefaItem.kt
@@ -1,8 +1,15 @@
 package com.example.gerenciador_tarefas.useri
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+//import androidx.compose.foundation.layout.*
+//import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,7 +21,7 @@ import com.example.gerenciador_tarefas.models.Tarefa
 
 @Composable
 fun TarefaItem(tarefa: Tarefa, onDelete: () -> Unit, onCheckChange: (Boolean) -> Unit, onClick: () -> Unit) {
-    Row(modifier = Modifier
+    Row (modifier = Modifier
         .fillMaxWidth()
         .padding(8.dp)
         .clickable(onClick = onClick),
